1d5864c9b4fcf24f317c5210d70931999722a524
Dan Rocha <drochag1@gmail.com>
2020-05-31 14:56:45 -0500

    Organization card

--- src/components/organization.js
+++ src/components/organization.js
@@ -1,20 +1,49 @@
 import React from 'react'
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { 
-  Card, 
-  CardImg,
-  CardBlock,
-  CardTitle,
-  CardSubtitle,
-  CardText,
-} from '@bootstrap-styled/v4';
-import { Github, Marker } from '../components/icons'
+import { Github, Marker, Link as LinkIcon } from '../components/icons'
+
+const Card = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgba(0, 0, 0, 0.125);
+  border-image: initial;
+  padding: 10px;
+  box-sizing: border-box;
+  align-items: center;
+`
+
+const CardImg = styled.img`
+  width: 100px;
+  margin-right: 10px;
+`
+
+const CardBlock = styled.div``
+const CardSubtitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`
+const CardText = styled.div``
 
 const CardInfo = styled.span`
   display: inline-block;
   text-decoration: none;
   margin-right: 5px;
+`
+
+const CardTitle = styled.div`
+  font-weight: bold;
+  margin-bottom: 10px;
+`
+
+const Link = styled.a`
+  text-decoration: none;
+  color: black;
 `
 
 const Organization = ({
@@ -27,13 +56,13 @@ const Organization = ({
     location,
   }
 }) => (
-  <Card width="50%">
+  <Card>
     <CardImg top src={avatarUrl} alt={name} />
     <CardBlock>
       <CardTitle>{name}</CardTitle>
       <CardSubtitle>
-        <CardInfo as="a" href={url} target="_blank"><Github /></CardInfo>
-        <CardInfo as="a" href={websiteUrl} target="_blank">{websiteUrl}</CardInfo>
+        <CardInfo as={Link} href={url} target="_blank"><Github /></CardInfo>
+        <CardInfo as={Link} href={websiteUrl} target="_blank"><LinkIcon /> {websiteUrl}</CardInfo>
         <CardInfo><Marker /> {location}</CardInfo>
       </CardSubtitle>
       <CardText>{description}</CardText>
